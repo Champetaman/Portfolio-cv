1295bea10de2be0e62125e83cf3fa2ccf7027209
Camilo Oviedo <caol29ster@gmail.com>
2024-04-10 19:42:02 +1000

    Change output to server on vercel to able analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,14 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-
+import vercel from "@astrojs/vercel/serverless";
 import robotsTxt from "astro-robots-txt";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), robotsTxt()],
-  site: 'https://camilooviedo.com/'
+  site: "https://camilooviedo.com/",
+  output: "server",
+  adapter: vercel({
+    webAnalytics: { enabled: true },
+  }),
 });
