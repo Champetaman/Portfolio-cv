c4ebfdcf05be047b4eefa05fc05f68bcb483a820
Camilo Oviedo <caol29ster@gmail.com>
2024-06-20 11:59:24 +1000

    Ajuste error despliegue

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,10 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-import vercel from "@astrojs/vercel/serverless";
 import robotsTxt from "astro-robots-txt";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), robotsTxt()],
   site: "https://camilooviedo.com/",
-  output: "hybrid",
-  adapter: vercel({
-    webAnalytics: { enabled: false },
-  }),
+  output: "static",
 });
