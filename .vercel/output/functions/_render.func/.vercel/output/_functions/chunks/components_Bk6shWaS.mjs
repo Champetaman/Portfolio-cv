ab42441376657a212b76b875de55bbfca4e0f1ec
Camilo Oviedo <caol29ster@gmail.com>
2024-06-20 11:45:04 +1000

    Eliminacion folder .vercel

--- .vercel/output/functions/_render.func/.vercel/output/_functions/chunks/components_Bk6shWaS.mjs
+++
@@ -1,5 +0,0 @@
-export { renderers } from '../renderers.mjs';
-
-const page = () => import('./pages/components_BVOgkJeD.mjs').then(n => n.e);
-
-export { page };
--- .vercel/output/functions/_render.func/.vercel/output/_functions/chunks/index_CFwzw2J9.mjs
+++
@@ -1,5 +0,0 @@
-export { renderers } from '../renderers.mjs';
-
-const page = () => import('./pages/index_2V1D2dGt.mjs');
-
-export { page };
