c68156536263b271c7ffa23fe699a74e4fd3c1d6
Camilo Oviedo <caol29ster@gmail.com>
2024-06-20 12:05:26 +1000

    Ajuste astro static

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,6 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
+import vercel from "@astrojs/vercel/static";
 import robotsTxt from "astro-robots-txt";
 
 // https://astro.build/config
@@ -7,4 +8,7 @@ export default defineConfig({
   integrations: [tailwind(), robotsTxt()],
   site: "https://camilooviedo.com/",
   output: "static",
+  adapter: vercel({
+    webAnalytics: { enabled: false },
+  }),
 });
