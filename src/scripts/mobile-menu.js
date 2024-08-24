6231941d6d733e5cd2fba00ddaf3ce4064c8849b
Camilo Oviedo <caol29ster@gmail.com>
2024-08-24 11:17:19 +1000

    update to work the mobile menu

--- src/scripts/mobile-menu.js
+++ src/scripts/mobile-menu.js
@@ -1,37 +1,25 @@
-/**
- * This script manages the functionality of the mobile menu.
- * It handles the opening and closing of the menu and ensures that
- * the menu is hidden when an item is clicked.
- */
+document.addEventListener("DOMContentLoaded", () => {
+  const menuToggle = document.getElementById("menu-toggle");
+  const menuClose = document.getElementById("menu-close");
+  const mobileMenu = document.getElementById("mobile-menu");
 
-document.addEventListener("astro:page-load", () => {
-  const menuToggle = document.getElementById("menu-toggle"); // Get the hamburger menu button element
-  const menuClose = document.getElementById("menu-close"); // Get the menu close button element
-  const mobileMenu = document.getElementById("mobile-menu"); // Get the mobile menu element
+  if (menuToggle && mobileMenu) {
+    menuToggle.addEventListener("click", () => {
+      mobileMenu.classList.toggle("hidden");
+    });
+  }
 
-  /**
-   * Opens the mobile menu by removing the 'hidden' class.
-   */
-  menuToggle.addEventListener("click", () => {
-    mobileMenu.classList.remove("hidden"); // Show the mobile menu
-  });
+  if (menuClose && mobileMenu) {
+    menuClose.addEventListener("click", () => {
+      mobileMenu.classList.add("hidden");
+    });
+  }
 
-  /**
-   * Closes the mobile menu by adding the 'hidden' class.
-   */
-  menuClose.addEventListener("click", () => {
-    mobileMenu.classList.add("hidden"); // Hide the mobile menu
-  });
-
-  // Get all the navigation items within the mobile menu
   const mobileNavItems = mobileMenu.querySelectorAll("a");
 
-  /**
-   * Close the mobile menu when any navigation item is clicked.
-   */
   mobileNavItems.forEach((navItem) => {
     navItem.addEventListener("click", () => {
-      mobileMenu.classList.add("hidden"); // Hide the mobile menu
+      mobileMenu.classList.add("hidden");
     });
   });
 });
